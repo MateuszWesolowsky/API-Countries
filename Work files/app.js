d823f541005f45f10113c4553f961bb70ea9c7d4
Mateusz <m.wesolowski.92@gmail.com>
2023-03-09 23:15:52 +0100

    first commit

---
+++ Work files/app.js
@@ -0,0 +1,36 @@
+import { renderCountriesList } from "./dom-elements.js";
+
+const API_URL = "https://restcountries.com/v3.1/all";
+
+
+// fetch(API_URL)
+// 	.then((res) => res.json())
+// 	.then((country) =>
+// 		country.map((country) => {
+// 			return {
+// 				capital: country.capital,
+// 				population: country.population,
+// 				name: country.name.common,
+// 				region: country.region,
+// 				flag: country.flags.png,
+// 			};
+// 		})
+// 	);
+
+// capital, population, name.common, region, flags.png
+
+(async () => {
+	const data = await (await fetch(API_URL)).json(); //skrócony zapis
+	// const resData = await data.json(); //dłuższy, czytelniejszy zapis
+	const countries = data.map((country) => {
+		return {
+			capital: country.capital,
+			population: country.population,
+			name: country.name.common,
+			region: country.region,
+			flag: country.flags.png,
+		};
+	});
+	// console.log(countries);
+	renderCountriesList(countries);
+})();
---
+++ Work files/dom-elements.js
@@ -0,0 +1,4 @@
+export const renderCountriesList = (countries) => {
+	const mainElement = document.querySelector('.main');
+	console.log(countries)
+};
