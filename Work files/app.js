74f72c26c1b69bf9f5e7ecbfbde4e62ce6f95063
Mateusz <m.wesolowski.92@gmail.com>
2023-03-15 22:35:20 +0100

    add filters function and update renderContries fun

--- Work files/app.js
+++ Work files/app.js
@@ -2,6 +2,29 @@ import { renderCountries } from "./dom-elements.js";
 
 const API_URL = "https://restcountries.com/v3.1/all";
 
+const searchInput = document.querySelector("#search");
+
+searchInput.addEventListener("input", (e) => {
+	let countryName = e.target.value;
+
+	fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+		.then((res) => res.json())
+		.then((country) => {
+			console.log(country);
+			const countries = country.map((country) => {
+				return {
+					capital: country.capital || "No Capital",
+					population: country.population,
+					name: country.name.common,
+					region: country.region,
+					flag: country.flags.png,
+				};
+			});
+			renderCountries(countries);
+		});
+});
+
+////////////////
 
 // fetch(API_URL)
 // 	.then((res) => res.json())
@@ -24,7 +47,7 @@ const API_URL = "https://restcountries.com/v3.1/all";
 	// const resData = await data.json(); //dłuższy, czytelniejszy zapis
 	const countries = data.map((country) => {
 		return {
-			capital: country.capital || 'No Capital',
+			capital: country.capital || "No Capital",
 			population: country.population,
 			name: country.name.common,
 			region: country.region,
--- Work files/dom-elements.js
+++ Work files/dom-elements.js
@@ -1,14 +1,15 @@
 
 
 const createInfoElement = (label, value) => {
-	const divCard = document.createElement('div');
+	const divInfo = document.createElement('div');
+	divInfo.classList.add('info');
 	const labelElement = document.createElement("strong");
 	const valueElement = document.createElement("span");
 	labelElement.textContent = `${label}: `;
 	valueElement.textContent = `${value}`;
-	divCard.appendChild(labelElement);
-	divCard.appendChild(valueElement);
-	return divCard;
+	divInfo.appendChild(labelElement);
+	divInfo.appendChild(valueElement);
+	return divInfo;
 }
 
 const createFlagElement = (country) => {
@@ -24,21 +25,17 @@ const createFlagElement = (country) => {
 const createCardElement = (country) => {
 	const countryCard = document.createElement("div");
 	countryCard.classList.add('card')
-
 	const countryName = document.createElement("strong");
+	countryName.classList.add('name');
 	countryName.innerText = country.name;
-
-	countryCard.appendChild(createFlagElement(country.flag));
-	countryCard.appendChild(countryName);
-	countryCard.appendChild(createInfoElement("Population", country.population.toLocaleString()));
-	countryCard.appendChild(createInfoElement("Region", country.region));
-	countryCard.appendChild(createInfoElement("Capital", country.capital));
+	countryCard.append(createFlagElement(country.flag),countryName,createInfoElement("Population", country.population.toLocaleString()),createInfoElement("Region", country.region), createInfoElement("Capital", country.capital))
 
 	return countryCard;
 };
 
 export const renderCountries = (countries) => {
 	const mainElement = document.querySelector(".main");
+	mainElement.innerHTML ='';
 	const country = countries.forEach((country) => {
 		mainElement.appendChild(createCardElement(country));
 	});
